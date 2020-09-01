278abec6d4c8dfdfedd4ee6f8ff0af2b69772abc
Nikolay Nikolaev Nenchev <58105310+NikolayNenchev@users.noreply.github.com>
2020-09-01 21:19:12 +0300

    Add files via upload

---
+++ expression1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class expression1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int calculate = (3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3;
+        System.out.println(calculate);
+    }
+}
---
+++ triangleOf55Stars1.java
@@ -0,0 +1,14 @@
+public class triangleOf55Stars1 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+        System.out.println("*******");
+        System.out.println("********");
+        System.out.println("*********");
+        System.out.println("**********");
+    }
+}
